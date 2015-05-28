944381783696be83d2993952d1fcbf1f9b7316b7
Aubrey Portwood <aubreypwd@gmail.com>
2015-05-28 12:22:34 -0700

    Remove console.log which was choking IE9.

--- assets/js/wds-hero-widget.js
+++ assets/js/wds-hero-widget.js
@@ -3,7 +3,7 @@
 	// Anything with data attached to it, slick it!
 	$( '.hero .sliders' ).each( function( i, v ) {
 		var speed = $( this ).attr( 'data-slider-speed' );
-		console.log( speed );
+
 		$( this ).slick( {
 			slidesToShow: 1,
 			slidesToScroll: 1,
