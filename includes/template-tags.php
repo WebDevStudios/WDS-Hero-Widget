e3060f1efed171a185ed9a8a271352ef62bcc00c
Aubrey Portwood <aubreypwd@gmail.com>
2015-06-03 13:17:38 -0700

    Fix template tag wrapper that would not work with echo off.

--- includes/template-tags.php
+++ includes/template-tags.php
@@ -11,5 +11,5 @@ if ( ! defined ( 'ABSPATH' ) ) {
  * @see WDS_Hero_Widget::mcf_hero() Argument defaults, etc.
  */
 function wds_hero( $args = array(), $content = false ) {
-	wds_hero_widget()->wds_hero( $args, $content );
+	return wds_hero_widget()->wds_hero( $args, $content );
 }
