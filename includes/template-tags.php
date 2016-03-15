83b303ba7eff6c70da028cc8b0bdfc53a46e11a8
Aubrey Portwood <aubreypwd@gmail.com>
2016-03-15 08:37:02 -0700

    The function is actually called wds_hero

--- includes/template-tags.php
+++ includes/template-tags.php
@@ -6,11 +6,11 @@ if ( ! defined ( 'ABSPATH' ) ) {
 }
 
 /**
- * Template tag wrapper for WDS_Hero_Widget::hero().
+ * Template tag wrapper for WDS_Hero_Widget::wds_hero().
  *
  * @since  1.0.0
  *
- * @see WDS_Hero_Widget::hero() Argument defaults, etc.
+ * @see WDS_Hero_Widget::wds_hero() Argument defaults, etc.
  */
 function wds_hero( $args = array(), $content = false ) {
 	return wds_hero_widget()->wds_hero( $args, $content );
